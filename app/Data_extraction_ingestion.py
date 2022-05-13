a2f77844472ef61898f80e02ba6a7970cc98f19e
SagarKolhe <kolhesagar45@gmail.com>
2022-05-13 10:16:35 +0530

    Code Update

--- app/Data_extraction_ingestion.py
+++ app/Data_extraction_ingestion.py
@@ -1,7 +1,7 @@
 """"
 This code extart the articles from 'https://api.spaceflightnewsapi.net/v3/documentation#/Article/get_articles' and
 
-ingest those into Elastic search docker container for further processing.
+ingest those into Elasticsearch docker container for further processing.
 
 """
 
@@ -27,30 +27,38 @@ except Exception as e:
     print("some modules are missing {}".format(e))
 
 try:
+    config = ConfigParser()
+    config.read('./app/app.ini')
+    es = Elasticsearch(config.get("Elasticsearch", "HOST2"))
+
 
     class DataExtractionIngestion:
+        """ Extract the data from public API and Insert it into Elasticsearch. """
+
         # Constructor of the class
         def __init__(self):
-            self.config = ConfigParser()
-            self.config.read('./app/app.ini')
-            self.es = Elasticsearch(self.config.get("Elasticsearch", "HOST2"))
-            # print(self.es.ping())
-
+            """ Required Variables. """
             self.data = None
 
         # For extracting the data from api.spaceflightnewsapi.net
         def DataExtarction(self):
-            count_result = requests.get(self.config.get("Public API", "ARTICLES_COUNT"))
+            """ Extract the data from the Public API and
+                save it to self.data
+            """
+            count_result = requests.get(config.get("Public API", "ARTICLES_COUNT"))
             print("Total number of Articles in the spaceflightnews are", count_result.json())
-            # print(count_result.json())
 
-            articles_result = requests.get(self.config.get("Public API", "ALL_ARTICLES"))
+            all_articles = config.get("Public API", "ARTICLES")
+            # print(all_articles +"?_limit="+str(count_result.json())+"&_sort=id")
+            articles_result = requests.get(all_articles + "?_limit=" + str(count_result.json()) + "&_sort=id")
             self.data = articles_result.json()
             logging.info("\nData Extraction is Done.......!!")
 
         # Data Processing
         def DataPreprocessing(self):
-            df = pd.read_csv(self.config.get("Files", "CSV_FILE"))
+            """ To check if there are NAN values in our data or not. If present remove those values."""
+
+            df = pd.read_csv(config.get("Files", "CSV_FILE"))
             print("\nchecking any feature has nan values: ", df.isna().sum())
             df = df.drop(['summary'], axis=1)
 
@@ -61,19 +69,32 @@ try:
             return df
 
         def saveExtectedDataToCSVFile(self):
+            """
+                Convert the data inside the self.data to pandas data frame and save it to CSV file
+            """
             df = pd.DataFrame(self.data)
-            df.to_csv(self.config.get("Files", "CSV_FILE"), index=False)
+            df.to_csv(config.get("Files", "CSV_FILE"), index=False)
             logging.info("\nData is saved to .csv File.....!!")
 
         def DataIngestion(self, final_data):
-            res = helpers.bulk(self.es, self.generator(final_data))
+            """ Bulk ingestion of the data to elasticsearch.
+
+                input: final_data. Elasticsearch supported data
+            """
+            res = helpers.bulk(es, self.generator(final_data))
             logging.info("\n Bulk Data Ingested to Elasticsearch successfully.......!!!")
 
         def generator(self, final_data):
+            """ Generates the data in Elasticsearch format.
+
+                input : final_data. Contains data which is not supported by the Elasticsearch.
+
+                output: convert each data in to Elasticsearch format
+             """
             for c, line in enumerate(final_data):
                 # print(c)
                 yield {
-                    "_index": "articles",
+                    "_index": "demo",
                     "_type": "doc",
                     "_id": line["id"],
                     "_source": {
@@ -93,15 +114,21 @@ try:
 except Exception as e:
     print("Something went wrong:-> {}  .......!!".format(e))
 
+
 def main():
-    obj1 = DataExtractionIngestion()
-    obj1.DataExtarction()
+    """ Create the object of the class DataExtractionIngestion () and
+        calls the required methods sequentially
+    """
 
-    obj1.saveExtectedDataToCSVFile()
+    data_extract_ingest = DataExtractionIngestion()
+    data_extract_ingest.DataExtarction()
 
-    data = obj1.DataPreprocessing()
+    data_extract_ingest.saveExtectedDataToCSVFile()
 
-    obj1.DataIngestion(data)
+    data = data_extract_ingest.DataPreprocessing()
 
-if __name__ == '__main__':
-    main()
+    data_extract_ingest.DataIngestion(data)
+
+
+# if __name__ == '__main__':
+#     main()
